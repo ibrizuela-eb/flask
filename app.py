db9505a352804433a06c5c7fb2ef9e4de5fcf93c
ibrizuela-eb <ibrizuela@eventbrite.com>
2023-04-19 11:28:20 -0300

    Allow cross origin just for dev purposes

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
 from flask import Flask, jsonify, request, redirect, render_template
+from flask_cors import CORS, cross_origin
 
 from repository.database import get_connection
 from repository.game_repository import GameRepository
 
 
 app = Flask("Games")
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route("/")
+@cross_origin()
 def home():
     return render_template('index.html')
 
 
 @app.route("/games/<int:id>")
+@cross_origin()
 def game_by_id(id):
     games = [GameRepository(get_connection()).get_game_by_id(id)]
     return jsonify(games=games), 200
 
 
 @app.route("/games")
+@cross_origin()
 def games():
     games = GameRepository(get_connection()).get_games()
     return jsonify(games=games), 200
 
 
 @app.route("/save_game", methods=["POST"])
+@cross_origin()
 def save_game():
     data = request.get_json()
     name = data["name"]
@@ -35,6 +42,7 @@ def save_game():
 
 
 @app.route("/update_game/<int:id>", methods=["PUT"])
+@cross_origin()
 def update(id):
     data = request.get_json()
     GameRepository(get_connection()).update_game(
@@ -47,6 +55,7 @@ def update(id):
 
 
 @app.route("/delete_game", methods=["POST"])
+@cross_origin()
 def delete_game():
     GameRepository(get_connection()).delete_game(data["id"])
     return redirect("/games")
